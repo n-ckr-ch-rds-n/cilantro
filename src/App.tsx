2f0553657517811a6a01f70b7aa30a88457ad752
nick_richardson <nick.richardson@rezonence.com>
2019-11-14 14:21:03 +0000

    Show a vertical color gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FullScreenCanvas} from "solandra-react/lib";
+import {LinearGradient, SCanvas} from "solandra";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FullScreenCanvas
+      playing
+      seed={1}
+      sketch={(p: SCanvas) => {
+        p.backgroundGradient(
+            new LinearGradient({
+              from: [0,0],
+              to: [0,1],
+              colors: [[0, {h: 240, s: 100, l: 20}], [1, {h: 10, s: 40, l: 90}]]
+            })
+        )
+      }}
+    />
+
   );
 }
 
